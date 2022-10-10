3e3ba1e735344c23ed7c6ba0b1b5c33713a8fb16
xuel <butter202209@163.com>
2022-10-10 20:08:13 +0800

    refactor.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Timer from "./Timer";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -8,36 +9,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
-class Timer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {seconds: 0}
-    }
-
-    tick(){
-        this.setState(state => ({
-            seconds: state.seconds + 1
-        }));
-    }
-
-    componentDidMount() {
-        this.interval = setInterval(() => this.tick(), 1000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
-
-    render() {
-        return (
-            <div>
-                Seconds: { this.state.seconds}
-            </div>
-        );
-    }
-}
-
 
 root.render(<Timer />);
